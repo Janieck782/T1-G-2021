fc84b97c96c92bd3e4c60edbf9392ddbb4186e32
Janieck782 <jeansandocastro@gmail.com>
2021-04-20 19:20:55 -0400

    Version 2.0

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
+function graph(){
 // crea un array con los nodos
-var nodes = new vis.DataSet([
+    var nodes = new vis.DataSet([
     //ej: {id: 1, label: "Node 1"},
       {id: 1, label: "Node 1"},
       {id: 2, label: "Node 2"},
@@ -25,18 +26,89 @@ var nodes = new vis.DataSet([
     };
     //Variable que almacena configuraciones del grafo
     var options = {
+      "nodes": {
+        
+        "shape": "circle",
+        
+      },
       edges:{
         arrows:{
           to:{
-            enabled: true,
+            enabled: false,
           }
         }
       },
       configure:{
-        enabled:false,
-        container:undefined,
+        enabled: false,
+        container: document.getElementById("optionsContainer"),
         showButton: true,
-      }
-    };
+      },
+      //permite interactuar con el grafo
+      "manipulation": {
+        "enabled": false,
+        "initiallyActive": false
+    },
+    
+  };
     //inicializa el grafo (Recuadro contenedor,Informacion de vertices y aristas, opciones del grafo)
     var network = new vis.Network(container, data, options);
+  }
+
+  function addNode() {
+    try {
+      nodes.add({
+        id: document.getElementById("node-id").value,
+        label: document.getElementById("node-label").value,
+      });
+    } catch (err) {
+      alert(err);
+    }
+  }
+
+  function updateNode() {
+    try {
+      nodes.update({
+        id: document.getElementById("node-id").value,
+        label: document.getElementById("node-label").value,
+      });
+    } catch (err) {
+      alert(err);
+    }
+  }
+  function removeNode() {
+    try {
+      nodes.remove({ id: document.getElementById("node-id").value });
+    } catch (err) {
+      alert(err);
+    }
+  }
+
+  function addEdge() {
+    try {
+      edges.add({
+        id: document.getElementById("edge-id").value,
+        from: document.getElementById("edge-from").value,
+        to: document.getElementById("edge-to").value,
+      });
+    } catch (err) {
+      alert(err);
+    }
+  }
+  function updateEdge() {
+    try {
+      edges.update({
+        id: document.getElementById("edge-id").value,
+        from: document.getElementById("edge-from").value,
+        to: document.getElementById("edge-to").value,
+      });
+    } catch (err) {
+      alert(err);
+    }
+  }
+  function removeEdge() {
+    try {
+      edges.remove({ id: document.getElementById("edge-id").value });
+    } catch (err) {
+      alert(err);
+    }
+  }
