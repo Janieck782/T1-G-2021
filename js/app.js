c2454689734d21bd93f77bcd09cd9ac09af46a60
Janieck782 <52087242+Janieck782@users.noreply.github.com>
2021-04-26 20:23:11 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,114 +1,114 @@
-function graph(){
-// crea un array con los nodos
-    var nodes = new vis.DataSet([
-    //ej: {id: 1, label: "Node 1"},
-      {id: 1, label: "Node 1"},
-      {id: 2, label: "Node 2"},
-      {id: 3, label: "Node 3"},
-      {id: 4, label: "Node 4"},
-      {id: 5, label: "Node 5"},
-      {id: 6, label: "Node 6"},
-    ]);
-    // crea un array con los vertices
-    var edges = new vis.DataSet([
-    //ej: {from: 1, to: 3, label:"peso de la arista/ponderado"},
-      {from: 1, to: 3},
-      {from: 1, to: 5},
-      {from: 1, to: 4},
-      {from: 5, to: 6},
-    ]);
-    // Crea el Contenedor
-    var container = document.getElementById("mynetwork");
-    //Junta nodos y edges para inicializar el grafo
-    var data = {
-      nodes: nodes,
-      edges: edges,
-    };
-    //Variable que almacena configuraciones del grafo
-    var options = {
-      "nodes": {
-        
-        "shape": "circle",
-        
-      },
-      edges:{
-        arrows:{
-          to:{
-            enabled: false,
-          }
-        }
-      },
-      configure:{
-        enabled: false,
-        container: document.getElementById("optionsContainer"),
-        showButton: true,
-      },
-      //permite interactuar con el grafo
-      "manipulation": {
-        "enabled": false,
-        "initiallyActive": false
-    },
-    
+var nodes, edges, network;
+
+// convenience method to stringify a JSON object
+function toJSON(obj) {
+  return JSON.stringify(obj, null, 4);
+}
+
+function addNode() {
+  try {
+    nodes.add({
+      id: document.getElementById("node-id").value,
+      label: document.getElementById("node-label").value,
+    });
+  } catch (err) {
+    alert(err);
+  }
+}
+
+function updateNode() {
+  try {
+    nodes.update({
+      id: document.getElementById("node-id").value,
+      label: document.getElementById("node-label").value,
+    });
+  } catch (err) {
+    alert(err);
+  }
+}
+function removeNode() {
+  try {
+    nodes.remove({ id: document.getElementById("node-id").value });
+  } catch (err) {
+    alert(err);
+  }
+}
+
+function addEdge() {
+  try {
+    edges.add({
+      id: document.getElementById("edge-id").value,
+      from: document.getElementById("edge-from").value,
+      to: document.getElementById("edge-to").value,
+    });
+  } catch (err) {
+    alert(err);
+  }
+}
+function updateEdge() {
+  try {
+    edges.update({
+      id: document.getElementById("edge-id").value,
+      from: document.getElementById("edge-from").value,
+      to: document.getElementById("edge-to").value,
+    });
+  } catch (err) {
+    alert(err);
+  }
+}
+function removeEdge() {
+  try {
+    edges.remove({ id: document.getElementById("edge-id").value });
+  } catch (err) {
+    alert(err);
+  }
+}
+
+function draw() {
+  // create an array with nodes
+  nodes = new vis.DataSet();
+  nodes.on("*", function () {
+    document.getElementById("nodes").innerText = JSON.stringify(
+      nodes.get(),
+      null,
+      4
+    );
+  });
+  nodes.add([
+    { id: "1", label: "Nodo 1" },
+    { id: "2", label: "Nodo 2" },
+    { id: "3", label: "Nodo 3" },
+    { id: "4", label: "Nodo 4" },
+    { id: "5", label: "Nodo 5" },
+  ]);
+
+  // create an array with edges
+  edges = new vis.DataSet();
+  edges.on("*", function () {
+    document.getElementById("edges").innerText = JSON.stringify(
+      edges.get(),
+      null,
+      4
+    );
+  });
+  edges.add([
+    { id: "1", from: "1", to: "2" },
+    { id: "2", from: "1", to: "3" },
+    { id: "3", from: "2", to: "4" },
+    { id: "4", from: "2", to: "5" },
+  ]);
+
+  // create a network
+  var container = document.getElementById("mynetwork");
+  var data = {
+    nodes: nodes,
+    edges: edges,
   };
-    //inicializa el grafo (Recuadro contenedor,Informacion de vertices y aristas, opciones del grafo)
-    var network = new vis.Network(container, data, options);
-  }
+  var options = {};
+  network = new vis.Network(container, data, options);
+}
 
-  function addNode() {
-    try {
-      nodes.add({
-        id: document.getElementById("node-id").value,
-        label: document.getElementById("node-label").value,
-      });
-    } catch (err) {
-      alert(err);
-    }
-  }
-
-  function updateNode() {
-    try {
-      nodes.update({
-        id: document.getElementById("node-id").value,
-        label: document.getElementById("node-label").value,
-      });
-    } catch (err) {
-      alert(err);
-    }
-  }
-  function removeNode() {
-    try {
-      nodes.remove({ id: document.getElementById("node-id").value });
-    } catch (err) {
-      alert(err);
-    }
-  }
-
-  function addEdge() {
-    try {
-      edges.add({
-        id: document.getElementById("edge-id").value,
-        from: document.getElementById("edge-from").value,
-        to: document.getElementById("edge-to").value,
-      });
-    } catch (err) {
-      alert(err);
-    }
-  }
-  function updateEdge() {
-    try {
-      edges.update({
-        id: document.getElementById("edge-id").value,
-        from: document.getElementById("edge-from").value,
-        to: document.getElementById("edge-to").value,
-      });
-    } catch (err) {
-      alert(err);
-    }
-  }
-  function removeEdge() {
-    try {
-      edges.remove({ id: document.getElementById("edge-id").value });
-    } catch (err) {
-      alert(err);
-    }
-  }
+window.addEventListener("load", () => {
+    draw();
+  });
+  
